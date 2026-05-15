6584689f52eae0932dd565c8cdcd5df665146153
hadelglbe1234567890-netizen <hadelglbe.1234567890@gmail.com>
2026-05-15 15:23:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,28 @@
-
-from telegram.ext import Application, CommandHandler, ContextTypes
-from telegram import Update
+from flask import Flask
+import threading
+from telegram.ext import Updater, CommandHandler
 
 TOKEN = "897131997"
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("البوت شغال 🚀")
+app = Flask(__name__)
 
-app = Application.builder().token(TOKEN).build()
+@app.route('/')
+def home():
+    return "Bot Running"
 
-app.add_handler(CommandHandler("start", start))
+def start(update, context):
+    update.message.reply_text("هلا! البوت شغال")
 
-print("Bot Running...")
-app.run_polling()
+def run_bot():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+
+    updater.start_polling()
+    updater.idle()
+
+threading.Thread(target=run_bot).start()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
